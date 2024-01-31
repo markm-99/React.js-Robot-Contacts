f0170586aaef53bbc1c0f92c3e1dd0b175194370
Mark Mathew <47173533+markm-99@users.noreply.github.com>
2024-01-31 02:13:29 -0800

    Delete UnitTest directory

--- UnitTest/item_database.py
+++
@@ -1,7 +0,0 @@
-class ItemDatabase:
-    def __init__(self) -> None:
-        pass
-
-    def get(self, item:str) -> float:
-        pass
-    
--- UnitTest/test_sample.py
+++
@@ -1,6 +0,0 @@
-# calculate total price of items in cart
-def func(x):
-    return x + 1
-
-def test_answer():
-    assert func(4) == 5
